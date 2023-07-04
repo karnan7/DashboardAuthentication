7e68869bfe413ef3f09468ef1d1a11067d2f9858
karnan7 <adithkayalad@gmail.com>
2023-07-04 19:46:47 +0530

    sidebar complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
 
+import { useContext } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import { UserContext } from '../../context/UserContext';
+import './home.scss'
+import { Navigate } from 'react-router-dom';
 const Home = () => {
+  const context = useContext(UserContext)
   return (
-    <div>Home</div>
+    <>
+    {context.user? (
+      <div className='home'>
+        <div className="sidebar-section">
+          <Sidebar />
+        </div>
+  
+        <div className="home-section">
+            <Navbar/>
+            homepage
+        </div>
+      </div>
+    ) : (
+      <Navigate to="/"/>
+    )}
+    </>
   )
 }
 
